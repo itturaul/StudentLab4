ecbade702995ab2c93b2a281284cb3a38dcb0a7a
antonioiugaa <antonio.iuga@ulbsibiu.ro>
2026-05-15 09:05:57 +0300

    Student Laborator 8

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -105,53 +105,6 @@ public class Application {
     }
 }
 
-public class Student {
-    private int nrMatricol;
-    private String nume;
-    private String prenume;
-    private String grupa;
-    private double nota;
-
-    public Student(int nrMatricol, String nume, String prenume, String grupa) {
-        this.nrMatricol = nrMatricol;
-        this.nume = nume;
-        this.prenume = prenume;
-        this.grupa = grupa;
-    }
-
-    public void setNota(double nota) {
-        this.nota = nota;
-    }
-
-    public int getNrMatricol() {
-        return nrMatricol;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Student)) return false;
-        Student student = (Student) o;
-        return nrMatricol == student.nrMatricol;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(nrMatricol);
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "nrMatricol=" + nrMatricol +
-                ", nume='" + nume + '\'' +
-                ", prenume='" + prenume + '\'' +
-                ", grupa='" + grupa + '\'' +
-                ", nota=" + nota +
-                '}';
-    }
-}
-
 public class AppStudenti {
 
     public static void main(String[] args) {
--- src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -1,86 +1,51 @@
 package ro.ulbs.proiectaresoftware.students;
 
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
-public class StudentExcelManager {
+public class Student {
+    private int nrMatricol;
+    private String nume;
+    private String prenume;
+    private String grupa;
+    private double nota;
 
-    public static void main(String[] args) {
-        String fileName = "laborator8_students.xlsx";
-
-        List<Student> listaInitiala = new ArrayList<>();
-        listaInitiala.add(new Student(101, "Ion", "Popescu", "311AC"));
-        listaInitiala.add(new Student(102, "Maria", "Ionescu", "312AC"));
-        listaInitiala.add(new Student(103, "Andrei", "Vasile", "311AC"));
-
-        exportStudentsToExcel(listaInitiala, fileName);
-
-        System.out.println("\nCitim studenții din fișierul generat:");
-        List<Student> listaCitita = importStudentsFromExcel(fileName);
-
-        listaCitita.forEach(System.out::println);
-    }
-    -
-    public static void exportStudentsToExcel(List<Student> studenti, String filePath) {
-        try (Workbook workbook = new XSSFWorkbook()) {
-            Sheet sheet = workbook.createSheet("Studenti");
-
-            Row headerRow = sheet.createRow(0);
-            String[] coloane = {"Nr. Matricol", "Prenume", "Nume", "Formatie"};
-            for (int i = 0; i < coloane.length; i++) {
-                Cell cell = headerRow.createCell(i);
-                cell.setCellValue(coloane[i]);
-            }
-
-            int rowIdx = 1;
-            for (Student s : studenti) {
-                Row row = sheet.createRow(rowIdx++);
-                row.createCell(0).setCellValue(s.getNumarMatricol());
-                row.createCell(1).setCellValue(s.getPrenume());
-                row.createCell(2).setCellValue(s.getNume());
-                row.createCell(3).setCellValue(s.getFormatieDeStudiu());
-            }
-
-            try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
-                workbook.write(fileOut);
-                System.out.println("Fișierul " + filePath + " a fost generat cu succes!");
-            }
-        } catch (Exception e) {
-            System.err.println("Eroare la export: " + e.getMessage());
-        }
+    public Student(int nrMatricol, String nume, String prenume, String grupa) {
+        this.nrMatricol = nrMatricol;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.grupa = grupa;
     }
 
-    public static List<Student> importStudentsFromExcel(String filePath) {
-        List<Student> listaStudenti = new ArrayList<>();
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
 
-        try (FileInputStream fis = new FileInputStream(new File(filePath));
-             Workbook workbook = new XSSFWorkbook(fis)) {
+    public int getNrMatricol() {
+        return nrMatricol;
+    }
 
-            Sheet sheet = workbook.getSheetAt(0);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return nrMatricol == student.nrMatricol;
+    }
 
-            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-                Row row = sheet.getRow(i);
-                if (row == null) continue;
+    @Override
+    public int hashCode() {
+        return Objects.hash(nrMatricol);
+    }
 
-                int matricol = (int) row.getCell(0).getNumericCellValue();
-                String prenume = row.getCell(1).getStringCellValue();
-                String nume = row.getCell(2).getStringCellValue();
-                String formatie = row.getCell(3).getStringCellValue();
-
-                listaStudenti.add(new Student(matricol, prenume, nume, formatie));
-            }
-
-        } catch (Exception e) {
-            System.err.println("Eroare la import: " + e.getMessage());
-        }
-
-        return listaStudenti;
+    @Override
+    public String toString() {
+        return "Student{" +
+                "nrMatricol=" + nrMatricol +
+                ", nume='" + nume + '\'' +
+                ", prenume='" + prenume + '\'' +
+                ", grupa='" + grupa + '\'' +
+                ", nota=" + nota +
+                '}';
     }
 }
-
